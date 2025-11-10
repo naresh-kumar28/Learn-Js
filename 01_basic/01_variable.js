bd19a63685db046ca47cfe5d7ae54daac5038b64
Naresh Kumar <nareshkumarsah780@gmail.com>
2025-11-10 13:32:25 +0000

    variables

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,14 @@
+const accountid = 12345
+let accountemail = "raja@gmail.com"
+var accountpwd = "raja@123"
+accountcity = "Purnea"
+let accountstate;
+
+// accountid = 456      const value change not allowed
+
+
+accountemail = "naresh@gmail.com"
+accountpwd = "kumar@123"
+accountcity = "patna"
+
+console.table([accountid,accountemail,accountpwd,accountcity,accountstate])
